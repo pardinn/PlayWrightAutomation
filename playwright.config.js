8320cabdccb35e412ad5960f3ce99ec543da7684
Victor Moraes <dt.victor@gmail.com>
2024-05-05 14:50:01 -0300

    Classes 1 to 15

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
@@ -11,7 +11,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,33 +20,38 @@ module.exports = defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5 * 1000,
+  },
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"], headless: false },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -76,4 +81,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+test.only("Browser Context Playwright test", async ({ browser }) => {
+  /**
+   *  Use this when you have cookies, plugins or other settings that you want to pass to the context,
+   *  otherwise, call the page directly.
+   **/
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const userName = page.getByLabel("Username:");
+  const signIn = page.getByRole("button", { name: "Sign In" });
+  const cardTitles = page.locator(".card-body a");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+
+  await userName.fill("rahulshetty");
+  await page.getByLabel("Password:").fill("learning");
+  await signIn.click();
+  console.log(await page.locator("[style*='block']").textContent());
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await cardTitles.first().textContent());
+  console.log(await cardTitles.nth(1).textContent());
+  const allTitles = cardTitles.allTextContents();
+  console.log(await allTitles);
+});
+
+test("Page Playwright test", async ({ page }) => {
+  // Same as above, but leveraging the default settings from Playwright
+  await page.goto("https://google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
+});
