0d52f541ba24f9daf5fc14b88bafb8201f3dc123
Victor Moraes <dt.victor@gmail.com>
2024-05-06 08:03:21 -0300

    Lesson 16: waiting for element with multiple match

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -5,6 +5,10 @@ test("Browser Context-Validating Error login", async ({ page }) => {
   await page.locator("#userEmail").fill("anshika@gmail.com");
   await page.locator("#userPassword").fill("Iamking@000");
   await page.locator("[value='Login']").click();
+  // method 1 for waiting (discouraged)
+  // await page.waitForLoadState("networkidle");
+  // method 2 for waiting
+  await page.locator(".card-body b").first().waitFor();
   const titles = await page.locator(".card-body b").allTextContents();
   console.log(titles);
 });
