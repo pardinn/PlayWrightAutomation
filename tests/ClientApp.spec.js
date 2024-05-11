ada46ace25f40da5ff35bf9f1fdedd97462fe18e
Victor Moraes <vrme@gft.com>
2024-05-11 10:25:56 -0300

    Dynamically find the product to buy

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("Browser Context-Validating Error login", async ({ page }) => {
+test.only("Client App login", async ({ page }) => {
+  const productName = "ADIDAS ORIGINAL";
+  const products = page.locator(".card-body");
   await page.goto("https://rahulshettyacademy.com/client");
   await page.locator("#userEmail").fill("anshika@gmail.com");
   await page.locator("#userPassword").fill("Iamking@000");
@@ -11,4 +13,15 @@ test("Browser Context-Validating Error login", async ({ page }) => {
   await page.locator(".card-body b").first().waitFor();
   const titles = await page.locator(".card-body b").allTextContents();
   console.log(titles);
+  const count = await products.count();
+  for (let i = 0; i < count; i++) {
+    await page.pause();
+    if ((await products.nth(i).locator("b").textContent()) === productName) {
+      await products
+        .nth(1)
+        .getByRole("Button", { name: "Add To Cart" })
+        .click();
+      break;
+    }
+  }
 });
