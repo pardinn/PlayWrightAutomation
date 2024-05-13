64c55f37a22f4df9a65b617fe367b06c063590f8
Victor Moraes <dt.victor@gmail.com>
2024-05-12 23:20:35 -0300

    Refactor API calls to utils

--- tests/WebApiPart1.spec.js
+++ tests/WebApiPart1.spec.js
@@ -1,39 +1,24 @@
 import { test, expect } from "@playwright/test";
+import APIUtils from "../utils/APIUtils";
 const loginPayload = { userEmail: "anshika@gmail.com", userPassword: "Iamking@000" };
 const orderPayload = { orders: [{ country: "Brazil", productOrderedId: "6581ca979fd99c85e8ee7faf" }] };
-let token;
-let orderId;
+let response;
 
 test.beforeAll(async ({ request }) => {
-  // Login API
-  const loginResponse = await request.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
-    data: loginPayload,
-  });
-  expect(loginResponse.ok()).toBeTruthy();
-  const loginResponseJson = await loginResponse.json();
-  token = loginResponseJson.token;
-  console.log(token);
-
-  // Create Order API
-  const orderResponse = await request.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
-    data: orderPayload,
-    headers: { Authorization: token, "Content-Type": "application/json" },
-  });
-  const orderResponseJson = await orderResponse.json();
-  console.log(orderResponseJson);
-  orderId = orderResponseJson.orders[0];
+  const apiUtils = new APIUtils(request, loginPayload);
+  response = await apiUtils.createOrder(orderPayload);
 });
 
 test("Client App login", async ({ page }) => {
   await page.addInitScript((value) => {
     window.localStorage.setItem("token", value);
-  }, token);
+  }, response.token);
   await page.goto("https://rahulshettyacademy.com/client");
-  console.log(orderId);
+  console.log(response.orderId);
 
   // Homework: Navigate to the Orders tab, find the order and click on View
   await page.getByRole("button", { name: "Orders" }).click();
-  await page.locator("tr").filter({ hasText: orderId }).getByRole("button", { name: "View" }).click();
-  await expect(page.getByText(`Order Id${orderId}`)).toBeVisible();
+  await page.locator("tr").filter({ hasText: response.orderId }).getByRole("button", { name: "View" }).click();
+  await expect(page.getByText(`Order Id${response.orderId}`)).toBeVisible();
   await expect(page.getByText("Country - Brazil").first()).toBeVisible();
 });
