4566c0d656a0ccc170b18a9f51c8e6bbb233bd98
Victor Moraes <dt.victor@gmail.com>
2024-05-18 21:17:02 -0300

    Manipulating Excel file in JS

---
+++ utils/ExcelUtils.js
@@ -0,0 +1,27 @@
+const ExcelJS = require("exceljs");
+
+async function writeExcel(searchText, replaceText, cellDelta, filePath) {
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet("Sheet1");
+  const output = await readExcel(worksheet, searchText);
+
+  const cellToReplace = worksheet.getCell(output.row + cellDelta.row, output.column + cellDelta.column);
+  cellToReplace.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
+}
+
+async function readExcel(worksheet, searchText) {
+  const output = { row: -1, column: -1 };
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      if (cell.value === searchText) {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
+    });
+  });
+  return output;
+}
+
+export default writeExcel;
