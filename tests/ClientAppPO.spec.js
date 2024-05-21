d7218405805d453e68ae825c66a56d358f02af5f
Victor Moraes <dt.victor@gmail.com>
2024-05-21 09:00:56 -0300

    Driving data with external JSON file

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,35 +1,24 @@
 import { test, expect } from "@playwright/test";
 import { POManager } from "../pageobjects/POManager";
+const dataset = JSON.parse(JSON.stringify(require("../utils/placeOrderTestData.json")));
 
 test("Client App login", async ({ page }) => {
   const pom = new POManager(page);
-  const username = "anshika@gmail.com";
-  const password = "Iamking@000";
-  const productName = "ADIDAS ORIGINAL";
   const loginPage = pom.getLoginPage();
   const dashboardPage = pom.getDashboardPage();
   const cartPage = pom.getCartPage();
   const checkoutPage = pom.getCheckoutPage();
   const ordersHistoryPage = pom.getOrdersHistoryPage();
-  const paymentInfo = {
-    cardNumber: "1234 5678 9012 3456",
-    expiryMonth: "10",
-    expiryYear: "24",
-    cvvCode: "123",
-    cardHolder: "Pardinn Hullkkan",
-    couponCode: "FEELINGLUCKY",
-  };
-  const country = "India";
 
   await loginPage.goTo();
-  await loginPage.validLogin(username, password);
-  await dashboardPage.addProductToCart(productName);
+  await loginPage.validLogin(dataset.username, dataset.password);
+  await dashboardPage.addProductToCart(dataset.productName);
   await dashboardPage.navigateToCart();
-  await cartPage.validateProductAddedToCart(productName);
+  await cartPage.validateProductAddedToCart(dataset.productName);
   await cartPage.checkout();
 
   // Homework. Fill out the Credit Card information
-  const orderId = await checkoutPage.placeOrder(paymentInfo, country, username);
+  const orderId = await checkoutPage.placeOrder(dataset.paymentInfo, dataset.country, dataset.username);
   console.log(orderId);
 
   // Homework: Navigate to the Orders tab, find the order and click on View
