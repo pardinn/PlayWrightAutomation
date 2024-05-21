9b3fb2ba333802362be852e3f9fb2ec5c3344a0e
Victor Moraes <dt.victor@gmail.com>
2024-05-21 09:20:02 -0300

    Running test with multiple data sets

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -2,28 +2,30 @@ import { test, expect } from "@playwright/test";
 import { POManager } from "../pageobjects/POManager";
 const dataset = JSON.parse(JSON.stringify(require("../utils/placeOrderTestData.json")));
 
-test("Client App login", async ({ page }) => {
-  const pom = new POManager(page);
-  const loginPage = pom.getLoginPage();
-  const dashboardPage = pom.getDashboardPage();
-  const cartPage = pom.getCartPage();
-  const checkoutPage = pom.getCheckoutPage();
-  const ordersHistoryPage = pom.getOrdersHistoryPage();
+for (const data of dataset) {
+  test(`Client App login - ${data.productName}`, async ({ page }) => {
+    const pom = new POManager(page);
+    const loginPage = pom.getLoginPage();
+    const dashboardPage = pom.getDashboardPage();
+    const cartPage = pom.getCartPage();
+    const checkoutPage = pom.getCheckoutPage();
+    const ordersHistoryPage = pom.getOrdersHistoryPage();
 
-  await loginPage.goTo();
-  await loginPage.validLogin(dataset.username, dataset.password);
-  await dashboardPage.addProductToCart(dataset.productName);
-  await dashboardPage.navigateToCart();
-  await cartPage.validateProductAddedToCart(dataset.productName);
-  await cartPage.checkout();
+    await loginPage.goTo();
+    await loginPage.validLogin(data.username, data.password);
+    await dashboardPage.addProductToCart(data.productName);
+    await dashboardPage.navigateToCart();
+    await cartPage.validateProductAddedToCart(data.productName);
+    await cartPage.checkout();
 
-  // Homework. Fill out the Credit Card information
-  const orderId = await checkoutPage.placeOrder(dataset.paymentInfo, dataset.country, dataset.username);
-  console.log(orderId);
+    // Homework. Fill out the Credit Card information
+    const orderId = await checkoutPage.placeOrder(data.paymentInfo, data.country, data.username);
+    console.log(orderId);
 
-  // Homework: Navigate to the Orders tab, find the order and click on View
-  await dashboardPage.navigateToOrders();
+    // Homework: Navigate to the Orders tab, find the order and click on View
+    await dashboardPage.navigateToOrders();
 
-  await ordersHistoryPage.viewOrder(orderId);
-  expect(await ordersHistoryPage.getOrderId()).toContainText(orderId);
-});
+    await ordersHistoryPage.viewOrder(orderId);
+    expect(await ordersHistoryPage.getOrderId()).toContainText(orderId);
+  });
+}
